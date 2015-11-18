77659c50d653acae00f1a320286c25f6408f0761
Leandro Lisboa Penz <lpenz@lpenz.org>
2015-11-18 15:46:30 -0200

    Add the busyna-run executable

---
+++ busyna-run/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"flag"
+	"os"
+
+	"github.com/lpenz/busyna/libbusyna"
+)
+
+func main() {
+	flag.Parse()
+
+	if flag.NArg() != 2 {
+		flag.Usage()
+		os.Exit(1)
+	}
+	rc := flag.Arg(0)
+	db := flag.Arg(1)
+	libbusyna.DbWrite(libbusyna.RcRun(libbusyna.RcParse(libbusyna.ChanFromFile(rc))), db)
+}
